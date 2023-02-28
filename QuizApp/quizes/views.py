3c9dedec114b5783ec36c27376af8c95b7dedbc8
Gabriel-Konarski <gabrys.konarski@wp.pl>
2023-02-28 19:16:32 +0100

    first quiz template

--- QuizApp/quizes/views.py
+++ QuizApp/quizes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Quiz, Question, Answer
 
-# Create your views here.
+
+def quizView(request, pk):
+    if request.method == 'GET':
+        quizz = Quiz.objects.get(id=pk)
+        questionn = Question.objects.filter(quiz=quizz.id)
+        answers = []
+        for quest in questionn:
+            answerr = quest.answer_set.all()
+            answers.append(answerr)
+
+        context = {"quiz": quizz, 'question': questionn, 'answers': answers}
+        return render(request, 'quiztemplate.html', context)
+
