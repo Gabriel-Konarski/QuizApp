40b2dce9dc10e3a007c9e04f7278935d9bedca70
Gabriel-Konarski <gabrys.konarski@wp.pl>
2023-02-28 20:45:44 +0100

    quiz sending with POST and fix templates files

--- QuizApp/quizes/views.py
+++ QuizApp/quizes/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Quiz, Question, Answer
 
 
 def quizView(request, pk):
+    quizz = get_object_or_404(Quiz, id=pk)
+    questionn = Question.objects.filter(quiz=quizz.id)
+
     if request.method == 'GET':
-        quizz = Quiz.objects.get(id=pk)
-        questionn = Question.objects.filter(quiz=quizz.id)
         answers = []
         for quest in questionn:
             answerr = quest.answer_set.all()
             answers.append(answerr)
 
         context = {"quiz": quizz, 'question': questionn, 'answers': answers}
-        return render(request, 'quiztemplate.html', context)
+        return render(request, 'quizes/quiztemplate.html', context)
 
+    elif request.method == 'POST':
+        points = 0
+        max_points = 0
+        data = request.POST
+        for quest in questionn:
+            max_points += 1
+            print(quest.id)
+            answer = get_object_or_404(Answer, id=data.get(str(quest.id)))
+            if answer.correct:
+                points += 1
+
+        return render(request, 'quizes/points.html', {'points': points, 'max_points': max_points})
