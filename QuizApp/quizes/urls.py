448912bf3c69ac70823b5218fa2b1facee2e23ad
Gabriel-Konarski <gabrys.konarski@wp.pl>
2023-03-13 19:19:38 +0100

    account details, quiz delete + quick order_by fix

--- QuizApp/quizes/urls.py
+++ QuizApp/quizes/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from . import views
+from django.urls import path, reverse_lazy
+from django.views.generic import DeleteView
 
+from . import views
+from .models import Quiz
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,4 +12,10 @@ urlpatterns = [
     path('category/<str:pk>/', views.categoryView, name='categories'),
     path('create_quiz/', views.createquizView, name='create_quiz'),
     path('add_question/<str:pk>/', views.add_question, name='add_question'),
+    path('account/', views.acountDetails, name='account'),
+    path('quiz/<int:pk>/delete/', DeleteView.as_view(
+                                                    model=Quiz,
+                                                    success_url=reverse_lazy('account'),
+                                                    template_name='quizes/generic_delete.html'
+                                                 ), name='quiz-delete'),
 ]
